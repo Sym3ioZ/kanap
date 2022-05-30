622811924fbc739ae398d09b25cc01cf4e36d2fc
Sym3ioZ <tortevois.benoit@gmail.com>
2022-05-30 15:06:59 +0200

    product.js advanced, display OK, still to handle button action

--- front/js/product.js
+++ front/js/product.js
@@ -17,7 +17,29 @@ fetch (`http://localhost:3000/api/products/${productId}`)
         console.log('erreur!');
     });
 
+// Creates product elements to display 
 function createProduct(product) {
-    document.getElementsByClassName('item__img').innerHTML = 
-        `<img src= "${product.imageUrl}" alt= "${product.altTxt}" />`;
+    let itemImage = document.querySelector('div.item__img');
+    let productImage = document.createElement('img');
+    productImage.setAttribute('src', `${product.imageUrl}`);
+    productImage.setAttribute('alt', `${product.altTxt}`);
+    itemImage.appendChild(productImage);
+
+    let productName = document.getElementById('title');
+    productName.textContent = `${product.name}`;
+
+    let productPrice = document.getElementById('price');
+    productPrice.textContent = `${product.price}`;
+
+    let productDescritpion = document.getElementById('description');
+    productDescritpion.textContent = `${product.description}`;
+
+    for (let color in product.colors) {
+        let colorChoice = document.getElementById('colors');
+        let productColor = document.createElement('option');
+        console.log(product.colors[color]);
+        productColor.setAttribute('value', `${product.colors[color]}`);
+        productColor.textContent = `${product.colors[color]}`;
+        colorChoice.appendChild(productColor);
+    }
 }
