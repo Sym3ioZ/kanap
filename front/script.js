6c671b8084317deffeab94fab7b0e0ef90442be0
Sym3ioZ <tortevois.benoit@gmail.com>
2022-05-30 11:06:30 +0200

    added test files // created script.js, fetch to display products on index.ihtml

---
+++ front/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let items = document.getElementById('items');
+
+fetch('http://localhost:3000/api/products')
+    .then(function(res) {
+    if (res.ok) { 
+        return res.json();
+    }
+    })
+    .then(function(value) {
+        console.log(value);
+        createProducts(value);
+    })
+    .catch(function(err) {
+        console.log('erreur!');
+    });
+
+// creates a product element in index.html for every value returned in fetch above
+function createProducts(products) {
+    for (let prod in products) {  
+        let homeProduct = document.createElement('div');
+        (items.appendChild(homeProduct)).innerHTML =
+            `<a href="./product.html">
+                <article>
+                    <img src= "${products[prod].imageUrl}" alt= "${products[prod].altTxt}" />
+                    <h3 class="productName">${products[prod].name}</h3>
+                    <p class="productDescription">${products[prod].description}</p>
+                </article>
+            </a>`;
+    };
+}
