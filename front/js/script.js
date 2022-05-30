044ad4d1fc55dbbcd20ce4ecd2019471df9a5407
Sym3ioZ <tortevois.benoit@gmail.com>
2022-05-30 11:31:51 +0200

    init product.js, updated href link in script.js

--- front/js/script.js
+++ front/js/script.js
@@ -16,12 +16,13 @@ fetch('http://localhost:3000/api/products')
         console.log('erreur!');
     });
 
-// creates a product element in index.html for every value returned in fetch above
+// creates a product element in index.html for every value returned by fetch above
 function createProducts(products) {
     for (let prod in products) {  
         let homeProduct = document.createElement('div');
+        homeProduct.style.width = "70%";
         (items.appendChild(homeProduct)).innerHTML =
-            `<a href="./product.html">
+            `<a href="./product.html?${products[prod]._id}">
                 <article>
                     <img src= "${products[prod].imageUrl}" alt= "${products[prod].altTxt}" />
                     <h3 class="productName">${products[prod].name}</h3>
